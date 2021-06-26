d2a911a9fe9c15cf4f273790952318222df48803
unknown <puntusiche@protonmail.com>
2021-06-27 00:57:55 +0300

    'exchange'

--- main.py
+++ main.py
@@ -138,7 +138,7 @@ def acception(message):
 @bot.message_handler(commands=['exchange'])
 def exchanger(message):
     rub = str(exchange.get_exchange_rub())
-    bot.send_message(message.chat.id, 'ton crystal= ' + rub + '₽')
+    bot.send_message(message.chat.id, 'TON Crystal = ' + rub + '₽')
 
 
 @bot.message_handler(regexp=regexps.newproduct)
